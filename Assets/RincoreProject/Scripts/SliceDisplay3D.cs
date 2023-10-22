d0dadefbaa5599f5079864283198b89d48b710e5
Tamir Koskas <tamirkoskas9@gmail.com>
2023-10-22 14:11:51 +0300

    RINGPA

--- Assets/RincoreProject/Scripts/SliceDisplay3D.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SliceDisplay3D : MonoBehaviour
-{
-    public MeshFilter limiterFilter;
-    public MeshRenderer limiterRenderer;
-}
